8b1e6ae7fdb9e23e90dd6514ffc44a71bffaa412
Topsan2002 <topsan2002@gmail.com>
2024-09-11 03:09:54 +0700

    [UPLOAD] Project

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Set .ENV with this project
+``` bash
+EUREKA_INSTANCE_HOSTNAME=localhost
+EUREKA_CLIENT_SERVICE_URL_DEFAULT_ZONE=http://localhost:8761/eureka
+PORT=8888
+```
---
+++ src/test/kotlin/com/fooddelivery/configserverfooddelivery/ConfigServerFoodDeliveryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fooddelivery.configserverfooddelivery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigServerFoodDeliveryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
